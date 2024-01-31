805755bab9848f19e0c724c879458da3c5cca639
Bubbl3Waffl3 <salahlfd0@gmail.com>
2024-01-31 18:43:14 +0100

    test from post endpoint added

---
+++ src/globalmsgAPI/Interfaces/IMessageContext.cs
@@ -0,0 +1,11 @@
+﻿using globalmsgAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace globalmsgAPI.Interfaces
+{
+    public interface IMessageContext
+    {
+        DbSet<Message> Messages { get; set; }
+        int SaveChanges();
+    }
+}
--- src/globalmsgAPI/Models/MessageContext.cs
+++ src/globalmsgAPI/Models/MessageContext.cs
@@ -1,13 +1,15 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using globalmsgAPI.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.InMemory;
 
 namespace globalmsgAPI.Models
 {
-    public class MessageContext : DbContext
+    public class MessageContext : DbContext, IMessageContext
     {
         public MessageContext(DbContextOptions<MessageContext> options) : base(options)
         {
-            
         }
+
         public DbSet<Message> Messages { get; set; }
     }
 }
